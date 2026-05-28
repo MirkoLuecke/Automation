2d8e8afafbaeb1b82f43c48a29faa6af2741999c
Mirko <mirko@luecke.co>
2026-05-28 20:24:36 +0200

    feat: add WorkflowPickerDialog with name and description columns

---
+++ com.example.automation.parent/com.example.automation/src/main/java/com/example/automation/WorkflowPickerDialog.java
@@ -0,0 +1,88 @@
+package com.example.automation;
+
+import java.util.List;
+
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+
+import com.example.automation.model.Workflow;
+
+public class WorkflowPickerDialog extends TitleAreaDialog {
+
+    private final List<Workflow> workflows;
+    private TableViewer viewer;
+    private Workflow result;
+
+    public WorkflowPickerDialog(Shell parent, List<Workflow> workflows) {
+        super(parent);
+        this.workflows = workflows;
+    }
+
+    @Override
+    public void create() {
+        super.create();
+        setTitle("Open Workflow");
+        setMessage("Select a workflow to open.");
+        getButton(OK).setEnabled(false);
+    }
+
+    @Override
+    protected boolean isResizable() { return true; }
+
+    @Override
+    protected Control createDialogArea(Composite parent) {
+        Composite area = (Composite) super.createDialogArea(parent);
+        viewer = new TableViewer(area, SWT.FULL_SELECTION | SWT.BORDER | SWT.SINGLE);
+        viewer.getTable().setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+        viewer.getTable().setHeaderVisible(true);
+        viewer.getTable().setLinesVisible(true);
+        viewer.setContentProvider(ArrayContentProvider.getInstance());
+
+        TableViewerColumn nameCol = new TableViewerColumn(viewer, SWT.NONE);
+        nameCol.getColumn().setText("Name");
+        nameCol.getColumn().setWidth(180);
+        nameCol.setLabelProvider(new ColumnLabelProvider() {
+            @Override public String getText(Object element) {
+                return ((Workflow) element).getDisplayName();
+            }
+        });
+
+        TableViewerColumn descCol = new TableViewerColumn(viewer, SWT.NONE);
+        descCol.getColumn().setText("Description");
+        descCol.getColumn().setWidth(250);
+        descCol.setLabelProvider(new ColumnLabelProvider() {
+            @Override public String getText(Object element) {
+                String desc = ((Workflow) element).getDescription();
+                return desc == null ? "" : desc;
+            }
+        });
+
+        viewer.setInput(workflows);
+
+        viewer.addSelectionChangedListener(e ->
+            getButton(OK).setEnabled(!viewer.getStructuredSelection().isEmpty()));
+
+        viewer.addDoubleClickListener(e -> {
+            if (!viewer.getStructuredSelection().isEmpty()) okPressed();
+        });
+
+        return area;
+    }
+
+    @Override
+    protected void okPressed() {
+        result = (Workflow) ((IStructuredSelection) viewer.getSelection()).getFirstElement();
+        super.okPressed();
+    }
+
+    public Workflow getResult() { return result; }
+}
