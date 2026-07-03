58a5609ef4a89bfc0ffd11af8e5966cc66f0f108
Mirko <mirko@luecke.co>
2026-07-03 08:12:00 +0200

    fix: add serialVersionUID to ArtifactoryException

--- com.example.automation.parent/com.example.automation/src/main/java/com/example/automation/ArtifactoryException.java
+++ com.example.automation.parent/com.example.automation/src/main/java/com/example/automation/ArtifactoryException.java
@@ -1,6 +1,7 @@
 package com.example.automation;
 
 public class ArtifactoryException extends Exception {
+    private static final long serialVersionUID = 1L;
     public ArtifactoryException(String message) { super(message); }
     public ArtifactoryException(String message, Throwable cause) { super(message, cause); }
 }
